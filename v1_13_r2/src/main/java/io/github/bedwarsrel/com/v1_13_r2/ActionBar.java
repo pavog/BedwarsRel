c4fefa535d722132de3dd7df97bec70fda2e3cca
Paul Vogel <evilcodes.dev@gmail.com>
2018-11-11 16:45:13 +0100

    Add v1_13_r2 and v1_13_r3

---
+++ v1_13_r2/src/main/java/io/github/bedwarsrel/com/v1_13_r2/ActionBar.java
@@ -0,0 +1,20 @@
+package io.github.bedwarsrel.com.v1_13_r2;
+
+import net.minecraft.server.v1_13_R2.ChatMessageType;
+import net.minecraft.server.v1_13_R2.IChatBaseComponent;
+import net.minecraft.server.v1_13_R2.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_13_R2.PacketPlayOutChat;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class ActionBar {
+
+  public static void sendActionBar(Player player, String message) {
+    String s = ChatColor.translateAlternateColorCodes('&', message.replace("_", " "));
+    IChatBaseComponent icbc = ChatSerializer.a("{\"text\": \"" + s + "\"}");
+    PacketPlayOutChat bar = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO);
+    ((CraftPlayer) player).getHandle().playerConnection.sendPacket(bar);
+  }
+
+}
